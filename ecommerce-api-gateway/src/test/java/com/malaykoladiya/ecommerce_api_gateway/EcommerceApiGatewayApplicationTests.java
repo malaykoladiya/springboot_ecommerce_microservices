bd60d90d53c591b2873b8697bad2f95f7a225155
Malay Koladiya <malaykoladiya@gmail.com>
2024-07-17 17:20:42 -0700

    implement Spring API Gateway

---
+++ ecommerce-api-gateway/src/test/java/com/malaykoladiya/ecommerce_api_gateway/EcommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malaykoladiya.ecommerce_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
