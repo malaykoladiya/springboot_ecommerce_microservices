d4410ca813871991de241b6d42bf689ab31a9481
Malay Koladiya <malaykoladiya@gmail.com>
2024-07-18 15:46:10 -0700

    Migration from OpenFeign to RestClient

--- order-microservice/src/main/java/com/malaykoladiya/micro_order_service/MicroOrderServiceApplication.java
+++ order-microservice/src/main/java/com/malaykoladiya/micro_order_service/MicroOrderServiceApplication.java
@@ -2,10 +2,8 @@ package com.malaykoladiya.micro_order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MicroOrderServiceApplication {
 
 	public static void main(String[] args) {
