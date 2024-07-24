b8d908ac4784955accf6278f7052dc8f9cdee658
Malay Koladiya <malaykoladiya@gmail.com>
2024-07-24 00:03:40 -0700

    Updated to add ConcurrentKafkaListenerContainerFactory

--- order-microservice/src/main/java/com/malaykoladiya/micro_order_service/config/ObservationConfig.java
+++ order-microservice/src/main/java/com/malaykoladiya/micro_order_service/config/ObservationConfig.java
@@ -5,8 +5,23 @@ import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
 @Configuration
+@RequiredArgsConstructor
 public class ObservationConfig {
+
+    private final ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory;
+
+    @PostConstruct
+    public void setObservationForKafkaTemplate(){
+        concurrentKafkaListenerContainerFactory.getContainerProperties().setObservationEnabled(true);
+    }
+
     @Bean
     ObservedAspect observedAspect(ObservationRegistry registry) {
         return new ObservedAspect(registry);
